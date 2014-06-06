ffcd4bae0e087ef001f40d6c4bf8035d25d29bb6
Remy Sharp <remy@remysharp.com>
2014-06-06 15:37:17 +0100

    status => statusCode

--- index.js
+++ index.js
@@ -3,15 +3,16 @@ var cache = []; // should be moved to something beyond memory
 
 var graveyard = function (req, res, next) {
   // if we have a hit on the cache, return the 410 early.
-  if (cache.indexOf(req.url) !== -1) {
+  var url = req.url.replace(/\?.*$/g, '');
+  if (cache.indexOf(url) !== -1) {
     return res.send(410);
   }
 
   // otherwise, attach to the headers being sent, and check if it's a 410
   res.on('header', function () {
-    if (res.status === 410) {
+    if (res.statusCode === 410) {
       // add to cache
-      cache.push(req.url);
+      cache.push(url);
     }
   });
 
